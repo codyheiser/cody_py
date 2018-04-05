c45f108042a5ea3a8c0170a7cab00b2d363da590
Cody Heiser <codyheiser49@gmail.com>
2018-04-05 17:03:55 -0500

    added max_char_repeats function and pandas options to utility functions

--- utilityfunctions.py
+++ utilityfunctions.py
@@ -1,7 +1,34 @@
+import pandas as pd
+
+# IMPORTANT: prevent truncation of long strings by Pandas
+#	useful for sequences embedded in dataframes
+pd.set_option('display.max_colwidth', -1)
+
+# convert appropriate strings ('TRUE', 'True', 'true') to boolean value (_True_)
 def str_to_bool(s):
-    if s == 'True':
+	'''
+	s = string that should be a boolean value (T or F)
+	output = corresponding boolean _True_ or _False_
+	'''
+    if s.title() == 'True':
          return True
-    elif s == 'False':
+    elif s.title() == 'False':
          return False
     else:
          raise ValueError("Cannot convert {} to a bool".format(s))
+
+# count homopolymer repeats in sequence
+def max_char_repeats(word):
+    '''
+    return maximum number of consecutive character repeats in string
+    '''
+    counts = []
+    count = 1
+    for a, b in zip(word, word[1:]):
+        if a == b:
+            count += 1
+        else:
+            counts.append(count)
+            count = 1
+    counts.append(count)
+    return max(counts)
